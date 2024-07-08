83e12e020e228f4541512ae8a88959da2a104264
JackRM10 <jrmclean10@gmail.com>
2024-07-08 16:47:17 -0700

    Project two functions
    index html contains two functions and an operator

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,8 +1,12 @@
 var Team = "Norwich City", Striker_1 = "Josh Sargent", Midfielder_1 = "Gabriel Sara", Defender_1 = "Jose Cordoba", Goalkeeper_1 = "Angus Gunn"
-document.write (Striker_1 ); //Requesting variable
+document.write (Striker_1); //Requesting variable
 document.write(3+3); //Calculate 3+3
 window.alert("Hello, world!"); //Box will apear when file is run saying "Hello, world!"
 var Sent1 = "This is the beginning of the string ", Sent2 = "and this is the end of the string"
 document.write (Sent1
-    + Sent2)
+    + Sent2)//Variable set up
+    
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+    }
 
