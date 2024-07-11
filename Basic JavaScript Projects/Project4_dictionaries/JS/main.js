c87a4ddc59dcf49d142cca678ea58de9b028f99a
JackRM10 <jrmclean10@gmail.com>
2024-07-11 12:37:25 -0700

    Project4
    Updated files, with complete assignment criteria

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,4 +1,4 @@
-//
+//Dicitionary for Guitar, result Model
 function my_Dictionary() {
     var Guitar = {
         Make: "Gibson",
@@ -8,19 +8,19 @@ function my_Dictionary() {
     };
     document.getElementById("Dictionary").innerHTML = Guitar.Model;
 }
-//
+//KVPS, two KVPS have the same key, result lowest down key displayed
 function my_KVPs() {
     var Classes = {
         ALG: "ALGEBRA 1",
         ALG: "ALGEBRA 2",
         HIS: "HISTORY",
         ENG: "ENGLISH LITERATURE",
-        PSY: "PSYCHOLOGY"
+        PSY: "PSYCHOLOGY",
     };
     document.getElementById("Classes").innerHTML = Classes.ALG;
 }
-//
-function my_Dictionary() {
+// Dictionary with delete statement, returns underfined result
+function my_Dictionary_Delete() {
     var Guitar = {
         Make: "Gibson",
         Model: "Les Paul",
@@ -28,5 +28,5 @@ function my_Dictionary() {
         Artist: "Jimmy Page",
     };
     delete Guitar.Model;
-    document.getElementById("Dictionary").innerHTML = Guitar.Model;
+    document.getElementById("DictionaryDelete").innerHTML = Guitar.Model;
 }
