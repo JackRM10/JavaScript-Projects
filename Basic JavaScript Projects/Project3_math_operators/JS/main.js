b40cf2bd0a3e3c85483468f188252caa7b31553b
JackRM10 <jrmclean10@gmail.com>
2024-07-11 11:04:52 -0700

    Updates to folder

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,35 +1,35 @@
-//Addition operator
-window.onload = function addition_Function() {
+//Function to add two numbers to return a result
+function addition_Function() {
     var addition = 2 + 2;
     document.getElementById("Math") .innerHTML = "2 + 2 = " + addition;
 }
-//Subtration operator
-window.onload = function subtraction_Function() {
+//Function to subtract two numbers to return a result
+function subtraction_Function() {
     var subtraction = 100 - 50;
     document.getElementById("Math-two") .innerHTML = "100 - 50 = " + subtraction;
 }
-//Multiplication operator
-window.onload = function multiplication_Function() {
+//Function to multiply two numbers to return a result
+function multiplication_Function() {
     var multiplication = 6 * 8;
     document.getElementById("Math-three") .innerHTML = "2 + 2 = " + multiplication;
 }
-//Division operator
-window.onload = function divion_Function() {
+//Function to divide two numbers to return a result
+function divion_Function() {
     var division = 48 / 6;
     document.getElementById("Math-four") .innerHTML = "48 / 6 = " + divison;
 }
-//Multiple math operators
-window.onload = function more_Math() {
+//Function to perform several calculations to return a result
+function more_Math() {
     var simple_Math = (1 + 2) * 10 / 2 - 5;
     document.getElementById("Math-five") .innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
-//Modulus operator (remainder)
-window.onload = function modulus_Operator() {
+//Function to return a remainder
+function modulus_Operator() {
     var simple_Math_one = 25 % 6;
     document.getElementById("Math-six").innerHTML = "When you divide 25 by 6 you have a remainder of: " + simple_Math_one;
 }
 //Negation Operator
-window.onload = function negation_Operator() {
+function negation_Operator() {
     var x = 10;
     document.getElementById("Math-seven") .innerHTML = -x;
 }
@@ -43,7 +43,7 @@ var Y = 5.25;
 Y--;
 document.write(Y);
 
-//Random function
+//Random function displayed in a window
 window.alert(Math.randon() * 100);
 
 //Math Obeject
---
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -0,0 +1,20 @@
+function my_Dictionary() {
+    var Guitar = {
+        Make: "Gibson",
+        Model: "Les Paul",
+        Type: "Black Beauty",
+        Artist: "Jimmy Page",
+    };
+    document.getElementById("Dictionary").innerHTML = Guitar.Model;
+}
+
+function my_KVPs() {
+    var Classes = {
+        ALG: "ALGEBRA 1",
+        ALG: "ALGEBRA 2",
+        HIS: "HISTORY",
+        ENG: "ENGLISH LITERATURE",
+        PSY: "PSYCHOLOGY"
+    };
+    document.getElementById("Classes").innerHTML = Classes.ALG;
+}
